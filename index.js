5cb752bf2cbd0e7af811afead1c360f6adb22f38
Samantha Goodman <antashma@gmail.com>
2020-11-05 18:30:44 -0500

    add dotenv and edit package.json for heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
 
 
-const PORT = 5000;
+
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`
         *** Server is listening on port ${PORT} ***
